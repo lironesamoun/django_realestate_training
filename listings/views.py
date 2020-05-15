e278fb595082e9d782b6a91cbd740d1ceef7bc9a
lirone <lirone.samoun@gmail.com>
2020-05-15 14:51:56 +0200

    search logic

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
@@ -11,6 +12,7 @@ def index(request):
     paged_listings = paginator.get_page(page)
     context = {
         'listings': paged_listings,
+
     }
     return render(request, 'listings/listings.html', context)
 
@@ -25,4 +27,42 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    # city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+    context = {
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'listings': queryset_list,
+    }
+    return render(request, 'listings/search.html', context)
